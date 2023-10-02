969a53806d63f448fc2604947a20449f4e5613a4
dabidgmz <david.gmzherrera28@gmail.com>
2023-10-02 12:39:41 -0600

    agregamos countdowntimer para que al iniciar se inicie en automatico la segunda vista

--- app/src/main/java/com/example/practica4/MainActivity.java
+++ app/src/main/java/com/example/practica4/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.practica4;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button b;
+public class MainActivity extends AppCompatActivity {
+    TextView timerTextView;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b = findViewById(R.id.btn1);
-        b.setOnClickListener(this);
+        timerTextView = findViewById(R.id.timerTextView);
+        startTimer();
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn1) {
-            startActivity(new Intent(this, MainActivity2.class));
-        }
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText("Tiempo restante: " + millisUntilFinished / 1000 + " segundos");
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        }.start();
     }
 }
